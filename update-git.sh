a8878b25a91e902a4ea52a210b72d905c9cfe32c
Mihail Dragnev <bymykel5@gmail.com>
2023-08-26 02:39:46 +0200

    chore: shell script for git

---
+++ update-git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git add .
+
+changed_files=$(git status --porcelain | awk '{print $2}')
+
+git diff --quiet && git diff --staged --quiet || git commit -m "[bot] Update | $(echo "$changed_files" | wc -l) files" -m "$(echo "$changed_files")"
+git push origin main
